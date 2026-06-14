5b37b0ab89b263d6119b15ed73c82dad00509cd3
diosadellafortuna <diosadellafortuna@gmail.com>
2026-06-14 13:45:49 +0700

    update

---
+++ frontend/src/main/java/cashflow/dto/TransactionResponse.java
@@ -0,0 +1,62 @@
+package cashflow.dto;
+
+public class TransactionResponse {
+
+    private Long id;
+    private String transactionType;
+    private Double amount;
+    private String date;
+    private String description;
+    private String detail;
+
+    public TransactionResponse() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTransactionType() {
+        return transactionType;
+    }
+
+    public void setTransactionType(String transactionType) {
+        this.transactionType = transactionType;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+}
