8ac7c0ea0af1cb78e0401b86e84afcb9a7643cd1
diosadellafortuna <diosadellafortuna@gmail.com>
2026-06-14 13:47:27 +0700

    update

---
+++ frontend/src/main/java/model/User.java
@@ -0,0 +1,36 @@
+package model;
+
+public class User {
+
+    private int userId;
+    private String userName;
+    private String userEmail;
+    private String userPassword;
+
+    public User(int userId,
+                String userName,
+                String userEmail,
+                String userPassword) {
+
+        this.userId = userId;
+        this.userName = userName;
+        this.userEmail = userEmail;
+        this.userPassword = userPassword;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+}
